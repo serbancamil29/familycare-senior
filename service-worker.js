366d47ee9ebf0d55c28c2424f4f6590b3a112c5a
serbancamil29 <serbancamil@gmail.com>
2026-07-12 20:29:21 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'familycare-senior-v4.0.0';
-const SHELL = ['/pages/senior-login.html','/pages/senior.html','/styles/common.css','/styles/senior-kiosk.css','/assets/logo.svg','/assets/icon-192.png','/assets/icon-512.png','/manifest.webmanifest','/offline.html'];
+const CACHE = 'familycare-senior-v4.0.1';
+const SHELL = ['/pages/senior-login.html','/pages/senior.html','/pages/caregiver.html','/styles/common.css','/styles/access-v4.css','/styles/senior-kiosk.css','/assets/logo.svg','/assets/icon-192.png','/assets/icon-512.png','/manifest.webmanifest','/offline.html'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('familycare-senior-') && key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
